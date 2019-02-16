c0d2f51c8c0f8588b40faeaa800ad17c5cc768db
Aquarius (Research center) <pmamontov@aq.ru>
2019-02-16 12:04:14 +0300

    Update run-httpd.sh

--- run-httpd.sh
+++ run-httpd.sh
@@ -4,5 +4,6 @@
 # context after restarting the container.  httpd won't start correctly
 # if it thinks it is already running.
 rm -rf /run/httpd/* /tmp/httpd*
+echo "SELECT date_part('year', created::date) as year, date_part('month', created::date) AS month, COUNT(name) FROM main_host GROUP BY year, month ORDER BY year, month; SELECT count (name) AS total FROM main_host; SELECT name,created FROM main_host;" | PGPASSWORD=admin psql -h tower.aq.lab -d awx -U awx -H > /var/www/html/index.html
 
 exec /usr/sbin/apachectl -DFOREGROUND
